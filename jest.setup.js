10e9c040958a22378244fa4065d998afbe493411
SvenFE <shensven2016@gmail.com>
2023-04-01 01:23:38 +0800

    chore: add react-native-reanimated

--- jest.setup.js
+++ jest.setup.js
@@ -2,6 +2,12 @@ import {jest} from '@jest/globals';
 
 jest.useFakeTimers();
 
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.default.call = () => {};
+  return Reanimated;
+});
+
 jest.mock('@react-navigation/stack', () => {
   return {
     createStackNavigator: jest.fn().mockReturnValue({
