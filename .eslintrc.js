582fd40139baf8eeb9caa49eb9f2e975009d75a7
SvenFE <shensven2016@gmail.com>
2023-04-02 15:09:18 +0800

    chore: add eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-native/no-inline-styles': 'off',
+  },
 };
