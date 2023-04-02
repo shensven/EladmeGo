f0e899d7de0687241908c991263ee46a8ca061bc
SvenFE <shensven2016@gmail.com>
2023-04-03 00:43:36 +0800

    chore: add react-native-bars

--- android/app/src/main/java/com/eladmego/MainActivity.java
+++ android/app/src/main/java/com/eladmego/MainActivity.java
@@ -5,6 +5,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.zoontek.rnbars.RNBars;
 
 public class MainActivity extends ReactActivity {
 
@@ -37,5 +38,6 @@ public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
+    RNBars.init(this, "dark-content");
   }
 }
