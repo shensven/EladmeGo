45582895c13a91482ad823325622b49cdd0f3670
SvenFE <shensven2016@gmail.com>
2023-04-03 03:52:58 +0800

    feat: statusbar style adaption on Welcome screen

--- src/screen/Welcome/Welcome.tsx
+++ src/screen/Welcome/Welcome.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
-import {View, Image, Dimensions} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, Image, Dimensions, useColorScheme} from 'react-native';
+import {useIsFocused} from '@react-navigation/native';
+import {useAppearance} from '@/utils/appearance';
 
 function Welcome() {
   const screenHeight = Dimensions.get('screen').height;
+  const colorScheme = useColorScheme();
+  const {themeScheme, setStatusBarStyle} = useAppearance();
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused) {
+      setStatusBarStyle('dark-content');
+    } else {
+      switch (themeScheme) {
+        case 'light':
+          setStatusBarStyle('dark-content');
+          break;
+        case 'dark':
+          setStatusBarStyle('light-content');
+          break;
+        case 'system':
+          setStatusBarStyle(colorScheme === 'dark' ? 'light-content' : 'dark-content');
+          break;
+        default:
+          const _: never = themeScheme;
+          throw new Error('Unexpected themeScheme: ' + _);
+      }
+    }
+  }, [isFocused]);
 
   return (
     <View style={{flex: 1, alignItems: 'center'}}>
