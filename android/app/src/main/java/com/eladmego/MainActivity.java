dbeefe857b9c9102a10faef0260f291d3bb6d82d
SvenFE <shensven2016@gmail.com>
2023-04-03 01:22:03 +0800

    fix: initialize the splash screen for Android

--- android/app/src/main/java/com/eladmego/MainActivity.java
+++ android/app/src/main/java/com/eladmego/MainActivity.java
@@ -5,6 +5,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.zoontek.rnbootsplash.RNBootSplash;
 import com.zoontek.rnbars.RNBars;
 
 public class MainActivity extends ReactActivity {
@@ -37,6 +38,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this);
     super.onCreate(null);
     RNBars.init(this, "dark-content");
   }
