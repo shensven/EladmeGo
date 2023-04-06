9022cdad1e9673e59d7d7c4177881c9911f1e05d
SvenFE <shensven2016@gmail.com>
2023-04-06 16:03:57 +0800

    refactor: smoothing of homeHeader

--- src/screen/Home/HeaderTitle.tsx
+++ src/screen/Home/HeaderTitle.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import {View} from 'react-native';
 import {SvgXml} from 'react-native-svg';
 import EladmeGo from '@/assets/homeTitle/EladmeGo.svg';
 
 function HeaderTitle() {
-  return (
-    <View
-      style={{
-        transform: [{scale: 0.8}],
-      }}>
-      <SvgXml xml={EladmeGo.toString()} />
-    </View>
-  );
+  return <SvgXml xml={EladmeGo.toString()} />;
 }
 
 export default HeaderTitle;
